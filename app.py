dc13371b683625e693046f347360a1dd18d0c6c0
Adair Esmir <adairopez776@gmail.com>
2025-07-21 03:02:48 -0600

    Fix: puerto para Render

--- app.py
+++ app.py
@@ -148,5 +148,10 @@ def editar(id):
         return render_template("editar.html", equipo=equipo)
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
